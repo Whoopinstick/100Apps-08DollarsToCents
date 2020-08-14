f82019a0a26702d8d3861c483189a82c1843ebf3
Whoopinstick <k.m.paul68@gmail.com>
2020-08-13 21:15:55 -0400

    Completed project

--- 08 - DollarsToCents/View/ContentView.swift
+++ 08 - DollarsToCents/View/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var dollarText = ""
+    
+    var dollarAmount: Double {
+        return Double(dollarText) ?? 0.0
+    }
+    
+    var dollarAmountInt: Int {
+        return Int(dollarAmount * 100)
+    }
+    
+    var quarter: Int {
+        return dollarAmountInt / 25
+    }
+    
+    var dime: Int {
+        return (dollarAmountInt - (quarter * 25)) / 10
+    }
+    
+    var nickel: Int {
+        return (dollarAmountInt - (quarter * 25) - (dime * 10)) / 5
+    }
+    
+    var penny: Int {
+        return (dollarAmountInt - (quarter * 25) - (dime * 10) - (nickel * 5)) / 1
+    }
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Section(header: Text("Enter an amount:")) {
+                TextField("0.00", text: $dollarText)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section(header: Text("Amount as change:")) {
+                Text("Quarters: \(quarter)")
+                Text("Dimes: \(dime)")
+                Text("Nickels: \(nickel)")
+                Text("Pennies: \(penny)")
+            }
+        }
     }
 }
 
